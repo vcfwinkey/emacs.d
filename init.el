fa7615d733e3ee22c74d08732593aa3ca81495a8
Winkey <winkey728@gmail.com>
2014-05-09 18:45:35 +0800

    commit temporary packages and init.el file

---
+++ init.el
@@ -0,0 +1,22 @@
+(custom-set-variables '(show-paren-mode t))
+
+(when (>= emacs-major-version 24)
+(setq package-archives '(("gnu" . "http://elpa.gnu.org/packages/")
+                         ("marmalade" . "http://marmalade-repo.org/packages/")
+                         ("melpa" . "http://melpa.milkbox.net/packages/")))
+)
+
+;; Save all tempfiles in $TMPDIR/emacs$UID/
+(if (eq system-type 'window-nt)
+    (defconst emacs-tmp-dir (format "f:/%s/" "temp"))
+  (defconst emacs-tmp-dir (format "~/%s/" "temp")))
+(setq backup-directory-alist
+    `((".*" . ,emacs-tmp-dir)))
+(setq auto-save-file-name-transforms
+    `((".*" ,emacs-tmp-dir t)))
+(setq auto-save-list-file-prefix
+    emacs-tmp-dir)
+
+(add-to-list 'load-path "~/.emacs.d/elpa/undo-tree-20140110.616")
+(require 'undo-tree)
+(global-undo-tree-mode)
