c29de0d2c6695bf1155a6f7bf9aaca2c05b5b78b
Winkey <winkey728@gmail.com>
2014-07-21 17:30:10 +0800

    modify the macros

--- init-macros.el
+++ init-macros.el
@@ -1,5 +1,9 @@
-(fset 'delete-one-line
-   (lambda (&optional arg) "Keyboard macro." (interactive "p") (kmacro-exec-ring-item (quote ("" 0 "%d")) arg)))
-(global-set-key "\C-x\C-kK" 'delete-one-line)
+(fset 'copy-whole-line-to-next-line
+   (lambda (&optional arg) "Keyboard macro." (interactive "p") (kmacro-exec-ring-item (quote ([1 100663328 5 134217847 return 25] 0 "%d")) arg)))
+(global-set-key "\M-N" 'copy-whole-line-to-next-line)
+
+(fset 'copy-whole-line-to-prev-line
+   (lambda (&optional arg) "Keyboard macro." (interactive "p") (kmacro-exec-ring-item (quote ([5 0 1 134217847 return 16 25] 0 "%d")) arg)))
+(global-set-key "\M-P" 'copy-whole-line-to-prev-line)
 
 (provide 'init-macros)
